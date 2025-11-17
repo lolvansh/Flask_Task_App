f427857154c5c187ebe381955b5d1c50009701b6
lolvansh <lolvansh577@gmail.com>
2025-11-17 23:03:23 +0530

    testing

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("hello world")
+# imprrorts
+from flask import Flask
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+
+
+#my app
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "testing"
+
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
